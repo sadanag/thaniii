2933a2eb0b8e0fa7d8b71a7b09990b6a8003d638
nagaaaaaaaaaa <nagaprasad9048@gmail.com>
2024-08-23 23:17:27 +0530

    frist

---
+++ addition.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Ask user for first number
+echo "Enter first number:4"
+
+# Ask user for second number
+echo "Enter second number:4"
+
+# Perform addition
+sum=$((4 + 4))
+
+# Display result
+echo "Sum: $sum"
